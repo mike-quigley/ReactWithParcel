6321ab972a23db49cf82b4791995c32d5a967607
Michael Quigley <MQuigley@xceleration.com>
2019-03-09 18:15:35 -0500

    initial commit, set up base reat project and temporary structure with parcel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+
+const RunApp = () => {
+  return <div>Testing</div>;
+};
+
+let App = document.querySelector('#app');
+
+ReactDOM.render(<RunApp />, App);
